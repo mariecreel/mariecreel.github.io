2a617de22c4a00658a6eb81146748e4996496f6c
mariecreel <nick.creel@protonmail.com>
2022-05-08 21:30:53 -0400

    expand aboutme

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -13,7 +13,8 @@ const AboutMe = ({className}) => {
       <p>
           More specifically, I&#39;m experienced with API development, mobile 
           app development, and accessible front-end development. I&#39;m eager
-           to grow by facing new challenges in novel problem spaces.</p>
+           to grow by facing new challenges in novel problem spaces,
+           and I strive to make a positive impact.</p>
     </div>
   );
 };
